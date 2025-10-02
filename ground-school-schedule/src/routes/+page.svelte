bab9fd882a81c3dd91a40d27315c3d0d942fa343
andrehebra <52555960+andrehebra@users.noreply.github.com>
2025-10-02 12:48:30 +0000

    made simple page with header

---
+++ ground-school-schedule/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { Heading, P } from 'flowbite-svelte';
+</script>
+
+<div class="container"><h1 class="title">FREE GROUND SCHOOL</h1></div>
+
+
+<style>
+    :global(h1) {
+        color: #39488e;
+        font-weight: bold;
+        font-size: 4rem;
+    }
+        :global(.container) {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%; /* Full width of the viewport */
+        height: 100vh; /* Full height of the viewport */
+        margin: 0;
+        padding: 0;
+    }
+</style>
