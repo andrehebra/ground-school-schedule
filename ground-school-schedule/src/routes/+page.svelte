4e796d71ebcb81d1864f212b62008ef1b3422989
Andre Hebra <andrehebra@gmail.com>
2025-10-02 09:22:53 -0400

    added some of the private lessons to array

--- ground-school-schedule/src/routes/+page.svelte
+++ ground-school-schedule/src/routes/+page.svelte
@@ -1,8 +1,51 @@
 <script>
     import { Heading, P } from 'flowbite-svelte';
+
+    const privateSchedule = [
+        {
+            lesson: "Pilot Documents",
+            tasks: ["Aircraft Documents", "Aircraft Inspections"],
+        },
+        {
+            lesson: "Preflight Inspection",
+            tasks: ["Checklist Usage", "Passenger Briefing", "Positive Exchange of Flight Controls"],
+        },
+        {
+            lesson: "Operation of Systems",
+            tasks: ["Primary Flight Controls", "Secondary Flight Controls"],
+        },
+        {
+            lesson: "Operation of Systems",
+            tasks: ["Powerplant", "Engine Oil", "Ignition"],
+        },
+        {
+            lesson: "Operation of Systems",
+            tasks: ["Fuel"],
+        },
+        {
+            lesson: "Operation of Systems",
+            tasks: ["Electrical"],
+        },
+        {
+            lesson: "Safety Procedures",
+            tasks: ["Radio Communications", "Taxi Procedures", "Runway Incursion Avoidance", "Cockpit Management"],
+        },
+        {
+            lesson: "Safety Procedures",
+            tasks: ["Collision Avoidance", "Practice Areas", "Fuel and Weather Requirements", "Minimum Safe Altitudes"],
+        },
+        {
+            lesson: "Aerodynamics",
+            tasks: ["Lift Production", "Drag Production", "Stalls", "Wake Turbulence Avoidance"],
+        },
+        
+    ];
+
 </script>
 
-<div class="container"><h1 class="title">FREE GROUND SCHOOL</h1></div>
+<div class="container">
+    <h1 class="title">GROUND SCHOOL</h1>
+</div>
 
 
 <style>
