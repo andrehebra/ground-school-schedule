79ff0209ef7f80391f96efb709e3378c5a2654c4
Andre Hebra <andrehebra@gmail.com>
2025-10-03 08:41:58 -0400

    added private pilot lessons from ground school 2.0 document on google drive

--- ground-school-schedule/src/routes/+page.svelte
+++ ground-school-schedule/src/routes/+page.svelte
@@ -3,42 +3,125 @@
 
     const privateSchedule = [
         {
-            lesson: "Pilot Documents",
-            tasks: ["Aircraft Documents", "Aircraft Inspections"],
+            lesson: "Student Pilot Regulations",
+            tasks: ["Documents", "Privileges and Limitations", "Endorsements"],
         },
         {
-            lesson: "Preflight Inspection",
-            tasks: ["Checklist Usage", "Passenger Briefing", "Positive Exchange of Flight Controls"],
+            lesson: "Aircraft",
+            tasks: ["Documents", "Required Inspections"],
         },
         {
-            lesson: "Operation of Systems",
+            lesson: "Preflight Procedures",
+            tasks: ["Preflight Inspection", "Checklist Usage - Types", "CRM/SRM", "Amplified Checklist", "Passenger Briefing"],
+        },
+        {
+            lesson: "Flight Controls",
             tasks: ["Primary Flight Controls", "Secondary Flight Controls"],
         },
         {
-            lesson: "Operation of Systems",
-            tasks: ["Powerplant", "Engine Oil", "Ignition"],
+            lesson: "Powerplant",
+            tasks: ["Engine", "Oil System", "Engine Failures"],
         },
         {
-            lesson: "Operation of Systems",
-            tasks: ["Fuel"],
+            lesson: "Ignition System",
+            tasks: ["Ignition System Overview", "Magnetos", "Spark Plugs"],
         },
         {
-            lesson: "Operation of Systems",
-            tasks: ["Electrical"],
+            lesson: "Fuel System",
+            tasks: ["Fuel System Overview", "Fuel Injection", "Fuel Types", "Emergency Management"],
         },
         {
-            lesson: "Safety Procedures",
-            tasks: ["Radio Communications", "Taxi Procedures", "Runway Incursion Avoidance", "Cockpit Management"],
+            lesson: "Electrical System",
+            tasks: ["Electrical System Overview", "Starter", "Alternator", "Emergency Management"],
         },
         {
-            lesson: "Safety Procedures",
-            tasks: ["Collision Avoidance", "Practice Areas", "Fuel and Weather Requirements", "Minimum Safe Altitudes"],
+            lesson: "Radio Communications",
+            tasks: ["Procedures", "Loss of Communication", "Light Gun Signals"],
+        },
+        {
+            lesson: "Taxi Procedures",
+            tasks: ["Overview of Proper Procedures", "Crosswind Taxi Procedures"],
+        },
+        {
+            lesson: "Runway Incursion Avoidance",
+            tasks: ["Cockpit Management", "Pilot Deviations"],
+        },
+        {
+            lesson: "Collision Avoidance",
+            tasks: ["Traffic Scanning Techniques", "See-and-Avoid"],
         },
         {
             lesson: "Aerodynamics",
-            tasks: ["Lift Production", "Drag Production", "Stalls", "Wake Turbulence Avoidance"],
+            tasks: ["Production of Lift", "Turns"],
         },
-        
+        {
+            lesson: "Aerodynamics",
+            tasks: ["Stalls", "Loss of Control"],
+        },
+        {
+            lesson: "Aerodynamics",
+            tasks: ["Production of Drag", "Wake Turbulence"],
+        },
+        {
+            lesson: "Aerodymaics",
+            tasks: ["Production of Thrust", "Left Turning Tendencies", "Climb Aerodynamics"],
+        },
+        {
+            lesson: "Stalls and Spins (Deep Dive)",
+            tasks: ["In depth analysis and understanding of stalls and spins"],
+        },
+        {
+            lesson: "Aircraft Stability",
+            tasks: ["Weight and Balance"],
+        },
+        {
+            lesson: "Performance and Limitations",
+            tasks: ["Pressure Altitude", "Density Altitude"],
+        },
+        {
+            lesson: "Performance and Limitations",
+            tasks: ["Performance Charts"],
+        },
+        {
+            lesson: "Traffic Patterns",
+            tasks: ["Approaches", "Go Arounds"],
+        },
+        {
+            lesson: "Airspace",
+            tasks: ["Class A, B, C, D, E", "Special Use Airspace"],
+        },
+        {
+            lesson: "Weather Theory",
+            tasks: ["Part 1 of 2"],
+        },
+        {
+            lesson: "Weather Theory",
+            tasks: ["Part 2 of 2"],
+        },
+        {
+            lesson: "Weather Products",
+            tasks: ["Weather Reports", "Weather Forecasts"],
+        },
+        {
+            lesson: "Cross-country Flight Planning",
+            tasks: ["Route Planning", "Navigation Logs", "Pilotage", "Dead Reckoning"],
+        },
+        {
+            lesson: "Human Factors Part 1",
+            tasks: ["Obtaining Medical Certificates", "Medical Certificates", "Hypoxia"],
+        },
+        {
+            lesson: "Human Factors Part 2",
+            tasks: ["Inner Ear", "Supplemental Oxygen Requirements"],
+        },
+        {
+            lesson: "Night Operations",
+            tasks: ["Night Vision", "Night Taxi", "Optical Illusions"],
+        },
+        {
+            lesson: "Private Pilot Privileges and Limitations",
+            tasks: ["Expense Sharing", "Compensation"],
+        },
     ];
 
     let privateClasses = [];
